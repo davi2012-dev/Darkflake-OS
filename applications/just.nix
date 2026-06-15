68971c09ec7dc771e6f35af45204660abd503aa7
davi2012-dev <DaviMigue@proton.me>
2026-06-15 10:41:44 -0300

    Update just.nix

--- applications/just.nix
+++ applications/just.nix
@@ -7,16 +7,16 @@ let
   '';
   njust = pkgs.symlinkJoin {
     name = "njust";
-    paths = [ pkgs.just pkgs.tmux ];
+    paths = [ pkgs.just pkgs.tmux pkgs.bash ];
     buildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
       mkdir -p $out/bin
       makeWrapper ${pkgs.just}/bin/just $out/bin/njust \
         --add-flags "--justfile ${justfileContent}" \
-        --set PATH ${lib.makeBinPath [ pkgs.tmux pkgs.curl ]}
+        --add-flags "--shell ${pkgs.bash}/bin/bash" \
+        --set PATH ${lib.makeBinPath [ pkgs.tmux pkgs.curl pkgs.bash ]}
     '';
   };
 in {
-  # Isso é o que um módulo NixOS deve retornar
   environment.systemPackages = [ njust ];
 }
