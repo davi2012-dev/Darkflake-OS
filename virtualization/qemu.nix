ba4d9966886798473aa2b368a95e8217e510c55b
davi2012-dev <DaviMigue@proton.me>
2026-06-15 14:22:23 -0300

    Create qemu.nix

---
+++ virtualization/qemu.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }: {
+
+  virtualisation.qemu = {
+    # 1. Performance Bruta e Aceleração de Hardware
+    forceAccel = true;         # Força o uso do KVM por hardware; se falhar, o build avisa
+    diskInterface = "virtio";   # Usa drivers VirtIO para leitura/escrita de disco ultra rápidas
+    enableSharedMemory = true; # Compartilhamento eficiente de RAM entre as camadas virtuais
+
+    # 2. Suporte a Integração (Opcional, mas ajuda muito no monitoramento)
+    guestAgent.enable = true;  # Permite que o host se comunique melhor com o agente interno da VM
+
+    # 3. Argumentos Extras do QEMU (Passa o processador real do seu PC para dentro da VM)
+    options = [
+      "-cpu host"              # Garante que as flags de virtualização aninhada do seu processador passem adiante
+    ];
+  };
+
+}
