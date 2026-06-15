64e1433299d0f35b8058485c6702eb0ded23543b
davi2012-dev <DaviMigue@proton.me>
2026-06-15 08:55:43 -0300

    Update boot.nix

--- system/boot.nix
+++ system/boot.nix
@@ -1,5 +1,5 @@
 { config, pkgs, lib, ... }: {
-  
+
   boot.loader = {
     systemd-boot.enable = lib.mkForce false;
     efi.canTouchEfiVariables = true;
@@ -8,21 +8,17 @@
   boot.lanzaboote = {
     enable = true;
     pkiBundle = "/var/lib/sbctl";
-    
-    autoGenerateKeys = {
-      enable = true;
-    };
-
-    autoEnrollKeys = {
-      enable = true;
-    };
+    autoGenerateKeys.enable = true;
+    autoEnrollKeys.enable = true;
   };
+
+  # ⚠️ LINHA OBRIGATÓRIA PARA O XEN (do PR #387)
+  boot.bootspec.extensions."org.xenproject.bootspec.v1".vmlinux = "${config.system.build.kernel.dev}/vmlinux";
 
   boot.plymouth.enable = true;
   boot.consoleLogLevel = 0;
   boot.initrd.verbose = false;
   boot.hardwareScan = true;
-
   boot.tmp = {
     cleanOnBoot = true;
     useTmpfs = true;
