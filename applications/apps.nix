adb9e1be50da47043cd2ab6b5770c47726f5e81f
davi2012-dev <DaviMigue@proton.me>
2026-06-16 10:37:56 -0300

    Update apps.nix

--- applications/apps.nix
+++ applications/apps.nix
@@ -59,7 +59,7 @@
     winboat
     # --- Utilitários e Organização ---
     #iredis
-    redisinsight
+    #redisinsight
     flameshot           # Screenshots
     localsend           # Transferir arquivos via Wi-Fi
     obsidian            # Suas notas
