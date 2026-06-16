343bc0a0889ec9136e52e16389a9f39558305da8
davi2012-dev <DaviMigue@proton.me>
2026-06-15 21:14:15 -0300

    Update bundle.nix

--- virtualization/bundle.nix
+++ virtualization/bundle.nix
@@ -1,48 +1,8 @@
-{ config, pkgs, modulesPath, ... }: {
-
-  # 1. Importação dos sub-módulos isolados
+{ ... }: {
   imports = [
-    "${modulesPath}/virtualisation/qemu-vm.nix"
     ./podman.nix
     ./libvirtd.nix
     ./waydroid.nix
     ./home-assistant.nix
-    ./qemu.nix
-    ./vswitch.nix
-    #./xen.nix 
-  ];
-  
-  # 2. Configurações Globais de Virtualização da VM
-  virtualisation = {
-    # Resolução e Tela (Para quando abrir a interface gráfica da VM)
-    graphics = true;
-    resolution = { x = 1920; y = 1080; };
-    spiceUSBRedirection.enable = true;
-    # Inicialização moderna, Firmware e Segurança Estilo Hardware Real
-    useEFIBoot = true;
-    useSecureBoot = true;
-    tpm.enable = true;               # Emula o chip TPM virtual (fundamental com o SecureBoot)
-
-    # Otimizações de compartilhamento de arquivos com o Host físico
-    mountHostNixStore = true;
-    #nixStore9pCache = "loose";       # Cache de alta performance na RAM para ler o host
-    useHostCerts = true;             # Herda os certificados SSL do host (evita erros de rede)
-    
-    # Comportamento do armazenamento temporário
-    writableStore = true;
-    writableStoreUseTmpfs = true;    # Grava modificações temporárias do OS direto na RAM
-
-    # Isolamento de aplicativos em modo Sandbox (AppVM) para o seu usuário
-    appvm = {
-      enable = true;
-      user = "davi"; 
-    };
-  };
-
-  # 3. Ferramentas utilitárias extras para gerenciamento de redes no terminal
-  environment.systemPackages = with pkgs; [
-    bridge-utils
-    iptables
-    swtpm                           
   ];
 }
