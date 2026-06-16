7b82b6263cd456e5c7ab3bfdc71459c124df9fce
davi2012-dev <DaviMigue@proton.me>
2026-06-16 17:28:29 -0300

    Update bundle.nix

--- virtualization/bundle.nix
+++ virtualization/bundle.nix
@@ -8,5 +8,5 @@
   virtualisation.appvm.enable = true;
   virtualisation.appvm.user = "davi";
   virtualisation.vswitch.enable = true;
-  virtualisation.vswitch.resetOnStart = true;
+  virtualisation.vswitch.resetOnStart = false;
 }
