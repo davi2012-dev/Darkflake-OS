3d6dc6b13430410923327801e050df3cfb13f0b6
davi2012-dev <DaviMigue@proton.me>
2026-06-16 11:32:23 -0300

    Update podman.nix

--- virtualization/podman.nix
+++ virtualization/podman.nix
@@ -148,6 +148,21 @@
         ];
       };
 
+      # 11. Home Assistant (Injetado com sucesso no ecossistema)
+      homeassistant = {
+        image = "ghcr.io/home-assistant/home-assistant:stable";
+        volumes = [
+          "/var/lib/homeassistant:/config"
+          "/etc/localtime:/etc/localtime:ro"
+        ];
+        environment = {
+          TZ = "America/Sao_Paulo";
+        };
+        extraOptions = [
+          "--network=host" # Crucial para o HA achar suas luzes/TVs no Wi-Fi
+        ];
+      };
+
     };
   };
 
