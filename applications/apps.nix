17997cf149fa315a902360186afed1265becaa72
davi2012-dev <DaviMigue@proton.me>
2026-06-17 17:18:55 -0300

    Update apps.nix

--- applications/apps.nix
+++ applications/apps.nix
@@ -8,7 +8,7 @@
 
   environment.systemPackages = with pkgs; [
     # --- Internet e Comunicação ---
-    firedragon-catppuccin-bin
+    librewolf
     vesktop             # Discord com melhorias (Vencord)
     beeper              # O app de chat universal moderno
     crow-translate      # Tradutor rápido
