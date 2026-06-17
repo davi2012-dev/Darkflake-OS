eef0b05fbdb516bbe1f4fcff50609eb224195f10
davi2012-dev <DaviMigue@proton.me>
2026-06-17 10:17:03 -0300

    Update caddy.nix

--- system/network/caddy.nix
+++ system/network/caddy.nix
@@ -4,74 +4,67 @@
   services.caddy = {
   enable = true;
   email = "DaviMigue@proton.me";  # Pode manter, mas não será usado
-  globalConfig = ''
-    tls internal
-  '';
 
-    # Defina os virtual hosts para cada serviço
-    virtualHosts = {
-      # Exemplo: nextcloud.seudominio.local
-      "nextcloud.Darkflake.local" = {
-        extraConfig = ''
-          reverse_proxy localhost:8085
-        '';
-      };
+  # Remove o globalConfig
 
-      "jellyfin.Darkflake.local" = {
-        extraConfig = ''
-          reverse_proxy localhost:8096
-        '';
-      };
-
-      "librechat.Darkflake.local" = {
-        extraConfig = ''
-          reverse_proxy localhost:3080
-        '';
-      };
-
-      "homarr.Darkflake.local" = {
-        extraConfig = ''
-          reverse_proxy localhost:8083
-        '';
-      };
-
-      "stirling.Darkflake.local" = {
-        extraConfig = ''
-          reverse_proxy localhost:8089
-        '';
-      };
-
-      "metube.Darkflake.local" = {
-        extraConfig = ''
-          reverse_proxy localhost:8081
-        '';
-      };
-
-      "netdata.Darkflake.local" = {
-        extraConfig = ''
-          reverse_proxy localhost:19999
-        '';
-      };
-
-      "ha.Darkflake.local" = {
-        extraConfig = ''
-          reverse_proxy localhost:8123
-        '';
-      };
-
-      # Portainer já tem HTTPS nativo (9443), mas você pode querer proxyar também
-      "portainer.Darkflake.local" = {
-        extraConfig = ''
-          reverse_proxy localhost:9443 {
-            transport http {
-              tls_insecure_skip_verify
-            }
+  virtualHosts = {
+    "nextcloud.Darkflake.local" = {
+      extraConfig = ''
+        tls internal
+        reverse_proxy localhost:8085
+      '';
+    };
+    "jellyfin.Darkflake.local" = {
+      extraConfig = ''
+        tls internal
+        reverse_proxy localhost:8096
+      '';
+    };
+    "librechat.Darkflake.local" = {
+      extraConfig = ''
+        tls internal
+        reverse_proxy localhost:3080
+      '';
+    };
+    "homarr.Darkflake.local" = {
+      extraConfig = ''
+        tls internal
+        reverse_proxy localhost:8083
+      '';
+    };
+    "stirling.Darkflake.local" = {
+      extraConfig = ''
+        tls internal
+        reverse_proxy localhost:8089
+      '';
+    };
+    "metube.Darkflake.local" = {
+      extraConfig = ''
+        tls internal
+        reverse_proxy localhost:8081
+      '';
+    };
+    "netdata.Darkflake.local" = {
+      extraConfig = ''
+        tls internal
+        reverse_proxy localhost:19999
+      '';
+    };
+    "ha.Darkflake.local" = {
+      extraConfig = ''
+        tls internal
+        reverse_proxy localhost:8123
+      '';
+    };
+    "portainer.Darkflake.local" = {
+      extraConfig = ''
+        tls internal
+        reverse_proxy localhost:9443 {
+          transport http {
+            tls_insecure_skip_verify
           }
-        '';
-      };
+        }
+      '';
     };
   };
-
-  # Certifique-se de que as portas 80 e 443 estão liberadas (já estão nas suas regras)
-  # networking.firewall.allowedTCPPorts já inclui 80 e 443, então está ok.
-}
+};
