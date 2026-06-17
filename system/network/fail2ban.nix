84d047c836d7171a25eeff0754d4413df0136757
davi2012-dev <DaviMigue@proton.me>
2026-06-16 21:29:49 -0300

    Update fail2ban.nix

--- system/network/fail2ban.nix
+++ system/network/fail2ban.nix
@@ -1,26 +1,28 @@
 { config, pkgs, ... }: {
-  services.fail2ban = {
+services.fail2ban = {
+  enable = true;
+  banaction = "nftables";   
+
+  # Adicione o caminho absoluto para evitar problemas de PATH
+  extraConfig = ''
+    [Definition]
+    nftables = /run/current-system/sw/bin/nft
+  '';
+
+  maxretry = 5;
+  bantime = "24h";
+  bantime-increment = {
     enable = true;
-    banaction =  "nftables-multiport";
-    
-    maxretry = 5; 
-    bantime = "24h"; 
-  
-    bantime-increment = {
-      enable = true;
-      multipliers = "1 2 4 8 16 32 64";
-      maxtime = "43800000000000h";
-    };
-    
-    jails = {
-      sshd.settings = {
-        enabled = true;
-        port = "ssh";
-        filter = "sshd";
-        maxretry = 3; 
-        backend = "systemd"; 
-      };
-      
-    };
+    multipliers = "1 2 4 8 16 32 64";
+    maxtime = "43800000000000h";
   };
-}
+
+  jails.sshd.settings = {
+    enabled = true;
+    port = "ssh";
+    filter = "sshd";
+    maxretry = 3;
+    backend = "systemd";
+    banaction = "nftables";   # reforça aqui também
+  };
+};
