1a9cbea0503e1dee54f3d2bb9ebeb4fc760d792e
davi2012-dev <DaviMigue@proton.me>
2026-06-21 17:59:56 -0300

    Update boot.nix

--- system/boot.nix
+++ system/boot.nix
@@ -13,6 +13,13 @@
     autoEnrollKeys.enable = true;
   };
 
+  # --- CONFIGURAÇÃO DE EMULAÇÃO (ARM E POWERPC) ---
+  boot.binfmt = {
+    emulatedSystems = [ "aarch64-linux" "armv7l-linux" "powerpc64le-linux" ];
+    addEmulatedSystemsToNixSandbox = true;
+    preferStaticEmulators = true;
+  };
+
   boot.plymouth.enable = true;
   boot.consoleLogLevel = 0;
   boot.initrd.verbose = false;
