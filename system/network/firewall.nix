cac591947a84a9e54adfd901508e361cfde70002
davi2012-dev <DaviMigue@proton.me>
2026-06-21 10:13:36 -0300

    Update firewall.nix

--- system/network/firewall.nix
+++ system/network/firewall.nix
@@ -5,7 +5,7 @@
     allowPing = true; 
     logRefusedConnections = true;
     rejectPackets = false; # Stealth mode ativo (DROP por padrão)
-    checkReversePath = "loose"; # Mantido loose para não quebrar o roteamento do Waydroid/VPNs
+    checkReversePath = "strict";
 
     # Portas declaradas perfeitamente
     allowedTCPPorts = [ 
@@ -73,8 +73,8 @@
     
     # Roteamento essencial para o Waydroid + Anti-Spoofing Mitigado
     "net.ipv4.ip_forward" = 1; 
-    "net.ipv4.conf.all.rp_filter" = 2;
-    "net.ipv4.conf.default.rp_filter" = 2;
+    "net.ipv4.conf.all.rp_filter" = 1;
+    "net.ipv4.conf.default.rp_filter" = 1;
     "net.ipv4.conf.all.accept_source_route" = 0;
     "net.ipv4.conf.default.accept_source_route" = 0;
 
