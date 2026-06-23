8ddb33ea7c7adc872fa80e0a7f2025d7eee25051
davi2012-dev <DaviMigue@proton.me>
2026-06-23 17:22:14 -0300

    Update home.nix

--- applications/home/home.nix
+++ applications/home/home.nix
@@ -37,7 +37,9 @@
   services.amberol.enableRecoloring = true;
   services.amberol.replaygain = "album";
   services.xsettingsd.enable = true;
-  
+  services.plan9port.plumber.enable = true;
+  services.plan9port.fontsrv.enable = true;
+  services.plan9port.package = pkgs.plan9port-wayland;
   # 5. Pacotes
   home.packages = with pkgs; [
     tree
