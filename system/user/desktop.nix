e908674f16d07205140efecba2f76faef94762a6
davi2012-dev <DaviMigue@proton.me>
2026-06-23 21:22:44 -0300

    Update desktop.nix

--- system/user/desktop.nix
+++ system/user/desktop.nix
@@ -3,16 +3,16 @@
 {
   # --- 1. Servidor Gráfico e Display Manager ---
   services.xserver.enable = true;
-  services.displayManager.cosmic-greeter.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.plasma-login-manager.enable = true;
 
-  # --- 2. Ambiente de Desktop (COSMIC) ---
-  services.desktopManager.cosmic.enable = true;
-  services.desktopManager.cosmic.xwayland.enable = true;
+  # --- 2. Ambiente de Desktop (KDE Plasma 6) ---
+  services.desktopManager.plasma6.enable = true;
 
   # --- 3. Otimizações de Hardware para AMD GPU ---
   hardware.amdgpu = {
     opencl.enable = true;
-    overdrive.enable = true; 
+    overdrive.enable = true;
   };
 
   # --- 4. Serviços do Sistema e Integração ---
@@ -22,23 +22,22 @@
   services.upower.enable = true;
   programs.fuse.enable = true;
   services.playerctld.enable = true;
-  programs.dconf.enable = true; 
+  programs.dconf.enable = true;
   services.xscreensaver.enable = true;
   security.rtkit.enable = true;
   security.polkit.enable = true;
   security.chromiumSuidSandbox.enable = true;
- 
+
   services.hardware.openrgb.enable = true;
   programs.cpu-energy-meter.enable = true;
   programs.coolercontrol.enable = true;
-  
+
   services.tailscale.enable = true;
   programs.feedbackd.enable = true;
   programs.mouse-actions.enable = true;
   programs.mouse-actions.autorun = true;
   programs.librepods.enable = true;
   services.spice-vdagentd.enable = true;
-
   nixowos.enable = true;
 
   services.earlyoom = {
@@ -52,17 +51,16 @@
     enableSSHSupport = true;
   };
 
-  # --- 5. Gerenciamento de Energia ---
   powerManagement = {
     enable = true;
     powertop.enable = true;
     cpuFreqGovernor = "schedutil";
   };
 
-  # --- 6. Portais XDG (COSMIC) ---
+  # --- Portais XDG para KDE ---
   xdg.portal = {
     enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-cosmic];
-    config.common.default = [ "cosmic" ];
+    extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
+    config.common.default = [ "kde" ];
   };
 }
