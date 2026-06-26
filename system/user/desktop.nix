cac50d2489d92ec044dae8a4fca800b4d62419e9
davi2012-dev <DaviMigue@proton.me>
2026-06-26 17:15:50 -0300

    Update desktop.nix

--- system/user/desktop.nix
+++ system/user/desktop.nix
@@ -5,16 +5,16 @@
   services.xserver.enable = true;
   services.displayManager.plasma-login-manager.enable = true;
 
-  # ---  Ambiente de Desktop (KDE Plasma 6) ---
+  # --- Ambiente de Desktop (KDE Plasma 6) ---
   services.desktopManager.plasma6.enable = true;
 
-  # --- 3. Otimizações de Hardware para AMD GPU ---
+  # --- Otimizações de Hardware para AMD GPU ---
   hardware.amdgpu = {
     opencl.enable = true;
     overdrive.enable = true;
   };
 
-  # --- Monitoramento de Saúde dos Discos ---
+  # --- Monitoramento de Saúde dos Discos (novo) ---
   services.smartd = {
     enable = true;
     autodetect = true;
@@ -32,8 +32,8 @@
     };
   };
 
-
-  services.libinput.enable = true;
+  # --- Serviços do Sistema e Integração ---
+  services.libinput.enable = true;     
   services.blueman.enable = true;
   services.fstrim.enable = true;
   services.upower.enable = true;
@@ -41,19 +41,22 @@
   services.playerctld.enable = true;
   programs.dconf.enable = true;
   services.xscreensaver.enable = true;
-  services.irqbalance.enable = true;
+  services.irqbalance.enable = true;                
   security.rtkit.enable = true;
   security.polkit.enable = true;
   security.chromiumSuidSandbox.enable = true;
+
   services.hardware.openrgb.enable = true;
   programs.cpu-energy-meter.enable = true;
   programs.coolercontrol.enable = true;
+
   services.tailscale.enable = true;
   programs.feedbackd.enable = true;
   programs.mouse-actions.enable = true;
   programs.mouse-actions.autorun = true;
+  programs.librepods.enable = true;                  
   services.spice-vdagentd.enable = true;
-  nixowos.enable = true;  
+  nixowos.enable = true;
 
   services.earlyoom = {
     enable = true;
@@ -72,6 +75,7 @@
     cpuFreqGovernor = "schedutil";
   };
 
+  # --- Portais XDG para KDE ---
   xdg.portal = {
     enable = true;
     extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
