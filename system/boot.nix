ecdb65c0760c7ef405979b9d9c3bad6f9c3bd276
davi2012-dev <DaviMigue@proton.me>
2026-06-27 09:36:47 -0300

    Update boot.nix

--- system/boot.nix
+++ system/boot.nix
@@ -1,16 +1,30 @@
 { config, pkgs, lib, ... }: {
 
   boot.loader = {
-    systemd-boot.enable = lib.mkForce false;
+    systemd-boot.enable = lib.mkForce false; 
     efi.canTouchEfiVariables = true;
     efi.efiSysMountPoint = "/boot";
   };
 
+  # --- SECURE BOOT (LANZABOOTE) ---
   boot.lanzaboote = {
     enable = true;
     pkiBundle = "/var/lib/sbctl";
     autoGenerateKeys.enable = true;
     autoEnrollKeys.enable = true;
+  };
+
+  # --- CONFIGURAÇÃO DO INITRD (SYSTEMD NO BOOT) ---
+  boot.initrd = {
+    enable = true;
+    systemd = {
+      enable = true; 
+      tpm2.enable = true;
+      tpm2.pcrphases.enable = true;
+      shutdownRamfs.enable = true;
+    };
+    includeDefaultModules = true;
+    verbose = false;
   };
 
   # --- CONFIGURAÇÃO DE EMULAÇÃO (ARM E POWERPC) ---
@@ -20,17 +34,17 @@
     preferStaticEmulators = true;
   };
 
+  # --- OUTRAS CONFIGURAÇÕES DO SISTEMA ---
   boot.bootspec.enableValidation = true;
-  boot.tmp.tmpfsHugeMemoryPages = "within_size";
-  boot.initrd.includeDefaultModules = true;
   boot.plymouth.enable = true;
   boot.consoleLogLevel = 0;
-  boot.initrd.verbose = false;
-  boot.initrd.systemd.enable = true;
   boot.hardwareScan = true;
+  
+  # Gerenciamento avançado de arquivos temporários na RAM (Performance)
   boot.tmp = {
     cleanOnBoot = true;
     useTmpfs = true;
     tmpfsSize = "50%";
+    tmpfsHugeMemoryPages = "within_size";
   };
 }
