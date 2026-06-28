347a134808ef1a7c67638f12d6ac75ff96a6ed2d
davi2012-dev <DaviMigue@proton.me>
2026-06-28 14:59:20 -0300

    Update default.nix

--- applications/home/niri/default.nix
+++ applications/home/niri/default.nix
@@ -5,13 +5,11 @@
 
 {
   imports = [
-    inputs.niri.homeModules.niri # Import Niri's home-manager module
     ./settings.nix # Your custom configuration files for Niri
     ./keybinds.nix
     ./rules.nix
     ./autostart.nix
     ./scripts.nix
-
     ./noctaliashell.nix
   ];
 }
