4078cb9c13fdd86fe3776a56436eea0ce1c45132
davi2012-dev <DaviMigue@proton.me>
2026-06-28 14:06:38 -0300

    Update desktop.nix

--- system/user/desktop.nix
+++ system/user/desktop.nix
@@ -5,8 +5,9 @@
   services.xserver.enable = true;
   services.displayManager.plasma-login-manager.enable = true;
 
-  # --- Ambiente de Desktop (KDE Plasma 6) ---
+  # --- Ambiente de Desktop ---
   services.desktopManager.plasma6.enable = true;
+  programs.niri.enable = true;
 
   # --- Otimizações de Hardware para AMD GPU ---
   hardware.amdgpu = {
