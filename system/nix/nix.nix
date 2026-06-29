1c70a8d840c7661ee3592e40e32b4e8d7b7ee655
davi2012-dev <DaviMigue@proton.me>
2026-06-29 18:14:04 -0300

    Update nix.nix

--- system/nix/nix.nix
+++ system/nix/nix.nix
@@ -1,22 +1,6 @@
 { config, pkgs, lib, inputs, ... }: {
 
-  # --- 1. Auto Upgrade ---
-  system.autoUpgrade = {
-    enable = true;
-    dates = "daily";
-    flake = "/etc/nixos#Darkflake";
-    operation = "boot";
-    flags = [
-      "--update-input" "nixpkgs"
-      "--update-input" "chaotic"
-      "--update-input" "home-manager"
-      "--update-input" "nixpkgs-unstable"
-      "--update-input" "nix-cachyos-kernel" 
-      "-L"
-    ];
-  };
-
-  # --- 2. NH: O Assistente Moderno ---
+  # ---  NH: O Assistente Moderno ---
   programs.nh = {
     enable = true;
     clean.enable = true;
@@ -24,12 +8,11 @@
     flake = "/etc/nixos";
   };
 
-  # --- 3. Nix Config & Performance ---
+  # ---  Nix Config & Performance ---
   nix.channel.enable = false;
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" "ca-derivations" "pipe-operators" "recursive-nix" "dynamic-derivations" "fetch-tree"  ];
     auto-optimise-store = true;
-    allow-impure = true;
     substituters = [
       "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
@@ -53,7 +36,7 @@
     connect-timeout = 5;
   };
 
-  # --- 5. Nix-LD (Suporte a Binários Externos) ---
+  # --- Nix-LD (Suporte a Binários Externos) ---
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     stdenv.cc.cc zlib fuse3 openssl icu nss curl expat glibc 
@@ -72,7 +55,7 @@
   programs.nixbit.repository = "https://github.com/davi2012-dev/Darkflake-OS";
   programs.nix-required-mounts.enable = true;
 
-  # --- 6. GNU Guix Integration ---
+  # ---  GNU Guix Integration ---
   services.guix = {
     enable = true;
     gc.enable = true;
