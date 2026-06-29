f55e12c5ff052a7b0d90b6d81deaae1097d44143
davi2012-dev <DaviMigue@proton.me>
2026-06-28 21:47:42 -0300

    Update Bluetooth.nix

--- system/hardware/Bluetooth.nix
+++ system/hardware/Bluetooth.nix
@@ -63,7 +63,6 @@
 
   # --- Regra para ADIOS (scheduler de I/O) ---
   ACTION=="add|change", KERNEL=="sd*|nvme*|mmcblk*", ATTR{queue/rotational}=="0", TEST{queue/scheduler}=="1", ATTR{queue/scheduler}="adios"
- '';
 
   # --- 4. System76 Scheduler (Ajustado Especialmente para o Kernel BORE) ---
   services.system76-scheduler = {
