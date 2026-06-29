8ceed38687d6008f8556d40c4bc7709df38d0c26
davi2012-dev <DaviMigue@proton.me>
2026-06-29 20:38:11 -0300

    Update apps.nix

--- applications/apps.nix
+++ applications/apps.nix
@@ -10,7 +10,6 @@
     # --- Internet e Comunicação ---
     vesktop             # Discord com melhorias (Vencord)
     beeper              # O app de chat universal moderno
-    guixpkgs.hello
     crow-translate      # Tradutor rápido
     tor-browser
 
