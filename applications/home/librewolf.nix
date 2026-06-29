9583ada3b96f5815768a891953c27916e5d31604
davi2012-dev <DaviMigue@proton.me>
2026-06-29 17:18:33 -0300

    Update librewolf.nix

--- applications/home/librewolf.nix
+++ applications/home/librewolf.nix
@@ -1,36 +1,6 @@
 { config, pkgs, lib, ... }:
 
 let
-  # Configuração do mecanismo de busca (SearXNG via HTTPS)
-  searchConfig = pkgs.writeText "search.json.mozlz4" ''
-    {
-      "version": 6,
-      "engines": [
-        {
-          "_metaData": {
-            "alias": "searxng"
-          },
-          "_name": "SearXNG",
-          "_shortName": "searxng",
-          "description": "Busca privada via SearXNG",
-          "iconURL": "https://search.darkflake.local/static/themes/simple/img/favicon.png",
-          "isDefault": true,
-          "order": 1,
-          "searchForm": "https://search.darkflake.local",
-          "searchUrlGetParams": "q={searchTerms}&categories=general&language=pt",
-          "suggestUrlGetParams": "q={searchTerms}",
-          "type": "application/x-sj"
-        }
-      ]
-    }
-  '';
-
-  setupSearch = pkgs.writeShellScriptBin "setup-librewolf-search" ''
-    PROFILE_DIR="${config.home.homeDirectory}/.mozilla/librewolf/darkflake"
-    mkdir -p "$PROFILE_DIR"
-    cp ${searchConfig} "$PROFILE_DIR/search.json.mozlz4"
-  '';
-
   librewolf-wrapped = pkgs.writeShellScriptBin "librewolf" ''
     exec ${pkgs.bubblewrap}/bin/bwrap \
       --unshare-all \
@@ -71,21 +41,11 @@ in
         "privacy.trackingprotection.enabled" = true;
         "identity.fxaccounts.enabled" = true;
         "sidebar.verticalTabs" = true;
-        
-        # SearXNG como mecanismo de busca padrão
-        "browser.search.defaultenginename" = "SearXNG";
-        "browser.search.order.1" = "SearXNG";
-        "browser.urlbar.placeholderName" = "SearXNG";
-        "browser.search.hiddenOneOffs" = "";
-        "browser.urlbar.suggest.searches" = true;
       };
     };
   };
 
-  home.packages = [ 
-    librewolf-wrapped 
-    setupSearch 
-  ];
+  home.packages = [ librewolf-wrapped ];
 
   xdg.desktopEntries.librewolf = {
     name = "LibreWolf";
@@ -102,9 +62,4 @@ in
       StartupNotify = "true";
     };
   };
-
-  # Configura a busca ao ativar o perfil
-  home.activation.setupLibreWolfSearch = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    $DRY_RUN_CMD ${setupSearch}/bin/setup-librewolf-search
-  '';
 }
