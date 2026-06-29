fff8ff4e6e32daa5a53902ea5e3fc5696b340e9f
davi2012-dev <DaviMigue@proton.me>
2026-06-29 13:53:15 -0300

    Update librewolf.nix

--- applications/home/librewolf.nix
+++ applications/home/librewolf.nix
@@ -1,1 +1,12 @@
+{ config, pkgs, ... }:
 
+{
+  programs.librewolf = {
+    enable = true;
+    settings = {
+      "webgl.disabled" = false; 
+      "privacy.resistFingerprinting" = true;
+      "privacy.trackingprotection.enabled" = true;
+    };
+  };
+}
