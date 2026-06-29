5b4aff4171891249c4ccc41f60a7ecc7e05a8395
davi2012-dev <DaviMigue@proton.me>
2026-06-29 13:54:30 -0300

    Update librewolf.nix

--- applications/home/librewolf.nix
+++ applications/home/librewolf.nix
@@ -3,10 +3,19 @@
 {
   programs.librewolf = {
     enable = true;
+    
+    # Configurações de preferências do usuário
     settings = {
-      "webgl.disabled" = false; 
+      "webgl.disabled" = false;
       "privacy.resistFingerprinting" = true;
       "privacy.trackingprotection.enabled" = true;
+    };
+
+    # Ativa as políticas corporativas (Enterprise Policies) para injetar a flag de certificados
+    policies = {
+      Certificates = {
+        ImportEnterpriseRoots = true; # No Firefox/LibreWolf a flag de política chama-se assim
+      };
     };
   };
 }
