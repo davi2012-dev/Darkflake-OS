e48215e9c5bdc3ddfc513f6975f7bea6a3abc83b
davi2012-dev <DaviMigue@proton.me>
2026-06-30 15:31:53 -0300

    Update neovim.nix

--- applications/home/neovim.nix
+++ applications/home/neovim.nix
@@ -3,6 +3,12 @@
 {
   programs.neovim = {
     enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    withNodeJs = true;
+    withPython3 = true;
+
     plugins = with unstable.vimPlugins; [
       neorg              # do unstable
       nvim-nio
@@ -12,9 +18,32 @@
       nvim-cmp
       nvim-treesitter.withAllGrammars
     ];
+
     extraLuaPackages = luaPkgs: with luaPkgs; [
       pathlib-nvim
       lua-utils-nvim
     ];
+
+    extraLuaConfig = ''
+      -- Configuração básica
+      vim.opt.number = true
+      vim.opt.relativenumber = true
+      vim.g.mapleader = " "
+
+      -- Configuração do Neorg (ativa o plugin!)
+      require('neorg').setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.norg.concealer"] = {},
+          ["core.norg.completion"] = {
+            config = { engine = "nvim-cmp" },
+          },
+          ["core.norg.qol.toc"] = {},
+          ["core.norg.journal"] = {
+            config = { workspace = "journal" },
+          },
+        },
+      }
+    '';
   };
 }
