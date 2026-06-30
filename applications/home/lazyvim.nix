360c6d04e20c6eb914741f7bd2a5f6f800dda6b2
davi2012-dev <DaviMigue@proton.me>
2026-06-30 16:09:38 -0300

    Update lazyvim.nix

--- applications/home/lazyvim.nix
+++ applications/home/lazyvim.nix
@@ -8,45 +8,36 @@
   programs.lazyvim = {
     enable = true;
 
-    # Instala dependências básicas (git, ripgrep, fd, etc.)
     installCoreDependencies = true;
 
-    # Extras (habilite os que você usa)
     extras = {
-      # Línguas
       lang.nix.enable = true;
       lang.python = {
         enable = true;
-        installDependencies = true;        # ruff, etc.
-        installRuntimeDependencies = true; # python3
+        installDependencies = true;
+        installRuntimeDependencies = true;
       };
       lang.go = {
         enable = true;
         installDependencies = true;
         installRuntimeDependencies = true;
       };
-      # lang.typescript.enable = true; # se precisar
     };
 
-    # Pacotes adicionais (opcional)
     extraPackages = with pkgs; [
-      nixd        # LSP para Nix
-      alejandra   # Formatador Nix
-      # outros que você queira
+      nixd
+      alejandra
     ];
 
-    # Plugins extras (ex: Neorg)
     plugins = {
-      # Exemplo: adicionar Neorg como plugin
       neorg = lazyvim.lib.lazyConfig {
         plugin = "nvim-neorg/neorg";
-        # Configuração opcional (pode ser Lua inline ou arquivo)
         opts = {
           load = {
-            ["core.defaults"] = {};
-            ["core.norg.concealer"] = {};
-            ["core.norg.qol.toc"] = {};
-            ["core.norg.journal"] = {
+            "core.defaults" = {};         
+            "core.norg.concealer" = {};
+            "core.norg.qol.toc" = {};
+            "core.norg.journal" = {
               config = { workspace = "journal"; };
             };
           };
@@ -54,7 +45,6 @@
       };
     };
 
-    # Configurações personalizadas (opcional)
     config = {
       options = ''
         vim.opt.relativenumber = true
@@ -64,8 +54,5 @@
         vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
       '';
     };
-
-    # Se tiver uma pasta com arquivos .lua de configuração, use:
-    # configFiles = ./my-lazyvim-config;
   };
 }
