d74b3e674b0556e9106722969c1c00d0d3425522
davi2012-dev <DaviMigue@proton.me>
2026-06-30 11:02:04 -0300

    Create sops.nix

---
+++ security/sops.nix
@@ -0,0 +1,11 @@
+{ config, lib, ... }:
+
+{
+  sops.secrets."user-davi-hashed-password" = {
+    sopsFile = ./secrets.yaml;
+  };
+
+  sops.secrets."user-guest-hashed-password" = {
+    sopsFile = ./secrets.yaml;
+  };
+}
