63d51e18730da63bf20410e327a78da32866e18d
davi2012-dev <DaviMigue@proton.me>
2026-06-30 15:39:14 -0300

    Update neovim.nix

--- applications/home/neovim.nix
+++ applications/home/neovim.nix
@@ -1,46 +1,45 @@
 { config, pkgs, lib, unstable, ... }:
 
 {
-programs.neovim = {
-  enable = true;
-  defaultEditor = false;
-  viAlias = true;
-  vimAlias = true;
-  withNodeJs = true;
-  withPython3 = true;
+  programs.neovim = {
+    enable = true;
+    defaultEditor = false;
+    viAlias = true;
+    vimAlias = true;
+    withNodeJs = true;
+    withPython3 = true;
 
-  plugins = with unstable.vimPlugins; [
-    neorg
-    nvim-nio
-    nui-nvim
-    plenary-nvim
-    neorg-telescope
-    nvim-cmp
-    nvim-treesitter.withAllGrammars
-  ];
+    plugins = with unstable.vimPlugins; [
+      neorg
+      nvim-nio
+      nui-nvim
+      plenary-nvim
+      neorg-telescope
+      nvim-cmp
+      nvim-treesitter.withAllGrammars
+    ];
 
-  extraLuaPackages = luaPkgs: with luaPkgs; [
-    pathlib-nvim
-    lua-utils-nvim
-  ];
+    extraLuaPackages = luaPkgs: with luaPkgs; [
+      pathlib-nvim
+      lua-utils-nvim
+    ];
 
-  extraLuaConfig = ''
-    -- Configuração básica
-    vim.opt.number = true
-    vim.opt.relativenumber = true
-    vim.g.mapleader = " "
+    extraLuaConfig = ''
+      vim.opt.number = true
+      vim.opt.relativenumber = true
+      vim.g.mapleader = " "
 
-    -- Carrega o Neorg (sem completion para evitar erro)
-    require('neorg').setup {
-      load = {
-        ["core.defaults"] = {},
-        ["core.norg.concealer"] = {},
-        -- ["core.norg.completion"] removido para evitar erro
-        ["core.norg.qol.toc"] = {},
-        ["core.norg.journal"] = {
-          config = { workspace = "journal" },
+      require('neorg').setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.norg.concealer"] = {},
+          -- ["core.norg.completion"] removido
+          ["core.norg.qol.toc"] = {},
+          ["core.norg.journal"] = {
+            config = { workspace = "journal" },
+          },
         },
-      },
-    }
-  '';
-};
+      }
+    '';
+  };
+}
