6164915bcd4d5e629939c0d19024840d748babf2
davi2012-dev <DaviMigue@proton.me>
2026-06-30 14:53:57 -0300

    Update persistence.nix

--- system/persistence.nix
+++ system/persistence.nix
@@ -1,1 +1,27 @@
+{ config, pkgs, lib, ... }:
 
+{
+  preservation.storage."/persist" = {
+    directories = [
+      "/var/lib"
+      "/etc/NetworkManager/system-connections"
+    ];
+    files = [
+      "/etc/machine-id"
+    ];
+
+    users.davi = {
+      directories = [
+        "Downloads"
+        "Documents"
+        ".config/sops/age"   # <-- Chave Age persistente
+        ".local/share/keyrings"
+        ".ssh"
+      ];
+      files = [
+        ".bashrc"
+        ".zshrc"
+      ];
+    };
+  };
+}
