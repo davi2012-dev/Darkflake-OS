2c0824370697027a217adc4680905befab29e974
davi2012-dev <DaviMigue@proton.me>
2026-07-02 18:53:03 -0300

    Update swap.nix

--- system/filesystem/swap.nix
+++ system/filesystem/swap.nix
@@ -1,29 +1,26 @@
 { config, lib, pkgs, ... }:
 
 {
-  # 1. ZRAM (Swap ultra-rápido na RAM)
   zramSwap = {
     enable = true;
     algorithm = "zstd";
-    memoryPercent = 50;
-    priority = 50; # Prioridade máxima para ser usado antes do disco
+    memoryPercent = 50;      
+    priority = 50;        
   };
 
-  # 2. ZVOL ZFS (Swap de 16 GB no SSD como segunda camada)
   swapDevices = [
     {
       device = "/dev/zvol/rpool/encrypted/system/swap";
-      priority = 10; # Prioridade menor que a ZRAM para ser usado apenas como "estepe"
+      priority = 10;      
     }
   ];
 
-  # 3. Ajustes do Kernel (Otimização para o combo ZRAM + ZVOL)
   boot.kernel.sysctl = {
-    # Com ZRAM, swappiness alto é ótimo.
     "vm.swappiness" = 100;
-    # Importante: mude para 1 (ou tire) se usar ZVOL! 
-    # '0' desativa o read-ahead completamente, o que é ótimo para ZRAM, 
-    # mas pode deixar o swap em disco (ZVOL) um pouco arrastado ao ler páginas contíguas.
-    "vm.page-cluster" = 1;
+    "vm.page-cluster" = 0;  
+    "vm.dirty_ratio" = 30;        
+    "vm.dirty_background_ratio" = 10;
+    "vm.dirty_expire_centisecs" = 6000;  #
+    "vm.dirty_writeback_centisecs" = 500;  
   };
 }
