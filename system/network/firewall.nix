8058d214505657ba42c9ec2a5ebc14c126bfa2e3
davi2012-dev <DaviMigue@proton.me>
2026-07-03 11:50:56 -0300

    Update firewall.nix

--- system/network/firewall.nix
+++ system/network/firewall.nix
@@ -2,56 +2,60 @@
   networking.nftables.enable = true;
   networking.firewall = {
     enable = true;
-    allowPing = true; 
+    allowPing = true;
     logRefusedConnections = true;
     rejectPackets = false;
     checkReversePath = "strict";
 
-    allowedTCPPorts = [ 
+    allowedTCPPorts = [
       22 53 80 139 443 445 631 4460 51820 8080 3000 8123 9090 8083 53317
     ];
 
-    allowedUDPPorts = [ 
+    allowedUDPPorts = [
       53 123 4460 631 5353 41641
     ];
 
     trustedInterfaces = [ "waydroid0" "tailscale0" "lo" "podman+" "veth+" "proton+" "wg+" "tun+" "pvpn+" ];
 
     extraInputRules = ''
-      ct state invalid drop
-
-      tcp flags & (fin|syn|rst|ack) == syn ct count over 500 drop comment "anti-syn-flood"
-      tcp flags & (fin|syn|rst|ack) == rst ct count over 20 drop comment "anti-rst-flood"
-      
-      tcp flags & (fin|syn|rst|psh|ack|urg) == 0 drop comment "anti-null-scan"
-      tcp flags & (fin|syn|rst|psh|ack|urg) == fin|syn|rst|psh|ack|urg drop comment "anti-xmas-scan"
-
+      type filter hook input priority filter; policy drop;
+      iifname { ${builtins.concatStringsSep "," config.networking.firewall.trustedInterfaces} } accept
+      ct state vmap { invalid : drop, established : accept, related : accept }
+      tcp flags & (fin|syn|rst|ack) == syn ct count over 500 drop
+      tcp flags & (fin|syn|rst|ack) == rst ct count over 20 drop
+      tcp flags & (fin|syn|rst|psh|ack|urg) == 0 drop
+      tcp flags & (fin|syn|rst|psh|ack|urg) == fin|syn|rst|psh|ack|urg drop
+      tcp dport { ${builtins.concatStringsSep "," (map toString config.networking.firewall.allowedTCPPorts)} } accept
+      udp dport { ${builtins.concatStringsSep "," (map toString config.networking.firewall.allowedUDPPorts)} } accept
       meta l4proto tcp ct state new tcp dport != { ${builtins.concatStringsSep "," (map toString config.networking.firewall.allowedTCPPorts)} } \
-        limit rate over 2/minute burst 3 packets drop comment "tarpit-silencioso"
+        limit rate over 2/minute burst 3 packets drop
+      icmp type echo-request accept
+      icmpv6 type echo-request accept
+      tcp flags & (fin|syn|rst|ack) == syn log prefix "refused connection: " level info
     '';
 
     extraForwardRules = ''
+      type filter hook forward priority filter; policy drop;
       ct state invalid drop
     '';
   };
 
   boot.kernel.sysctl = {
-    # Redes e Performance Extrema
     "net.core.default_qdisc" = "fq";
     "net.ipv4.tcp_congestion_control" = "bbr3";
     "net.ipv4.tcp_fastopen" = 3;
     "net.ipv4.tcp_sack" = 1;
     "net.ipv4.tcp_window_scaling" = 1;
     "net.ipv4.tcp_frto" = 2;
-    
-    # Proteção ICMP
-    "net.ipv4.icmp_echo_ignore_all" = 0; 
-    "net.ipv4.icmp_echo_ignore_broadcasts" = 1; 
-    "net.ipv4.icmp_ignore_bogus_error_responses" = 1; 
-    "net.ipv4.icmp_ratelimit" = 100; 
-    
-    # Roteamento
-    "net.ipv4.ip_forward" = 1; 
+    "net.core.rmem_max" = 134217728;
+    "net.core.wmem_max" = 134217728;
+    "net.ipv4.tcp_rmem" = "4096 87380 134217728";
+    "net.ipv4.tcp_wmem" = "4096 65536 134217728";
+    "net.ipv4.icmp_echo_ignore_all" = 0;
+    "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
+    "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
+    "net.ipv4.icmp_ratelimit" = 100;
+    "net.ipv4.ip_forward" = 1;
     "net.ipv4.conf.all.rp_filter" = 1;
     "net.ipv4.conf.default.rp_filter" = 1;
     "net.ipv4.conf.all.accept_source_route" = 0;
@@ -62,38 +66,32 @@
     "net.ipv4.tcp_max_orphans" = 65536;
     "net.ipv4.ip_local_reserved_ports" = "22,53,80,139,443,445,631,4460,51820,8080,3000,8123,9090,8083,53317";
     "net.ipv4.ip_local_port_range" = "1024 65000";
-    "net.core.somaxconn" = 8192; 
-
-    # Logs
+    "net.core.somaxconn" = 8192;
     "net.ipv4.conf.all.log_martians" = 1;
     "net.ipv4.conf.default.log_martians" = 1;
-
-    # Redirecionamentos
     "net.ipv4.conf.all.send_redirects" = 0;
     "net.ipv4.conf.default.send_redirects" = 0;
     "net.ipv4.conf.all.accept_redirects" = 0;
     "net.ipv4.conf.default.accept_redirects" = 0;
-    "net.v6.conf.all.accept_redirects" = 0;
-    "net.v6.conf.default.accept_redirects" = 0;
-    "net.v6.conf.all.forwarding" = 1; 
-    "net.v6.conf.all.accept_source_route" = 0;
-    "net.v6.conf.default.accept_source_route" = 0;
+    "net.ipv6.conf.all.accept_redirects" = 0;
+    "net.ipv6.conf.default.accept_redirects" = 0;
+    "net.ipv6.conf.all.forwarding" = 1;
+    "net.ipv6.conf.all.accept_source_route" = 0;
+    "net.ipv6.conf.default.accept_source_route" = 0;
     "net.ipv4.conf.all.secure_redirects" = 0;
     "net.ipv4.conf.all.shared_media" = 0;
     "net.ipv4.igmp_max_memberships" = 100;
-
-    # SYN Flood
     "net.core.netdev_max_backlog" = 250000;
     "net.ipv4.tcp_syncookies" = 1;
     "net.ipv4.tcp_synack_retries" = 1;
     "net.ipv4.tcp_syn_retries" = 2;
-    "net.ipv4.tcp_max_syn_backlog" = 65536; 
-    "net.ipv4.tcp_rfc1337" = 1;              
-    
-    # Gerenciamento de conexões
+    "net.ipv4.tcp_max_syn_backlog" = 65536;
+    "net.ipv4.tcp_rfc1337" = 1;
     "net.ipv4.tcp_fin_timeout" = 30;
     "net.ipv4.tcp_orphan_retries" = 3;
     "net.ipv4.tcp_timestamps" = 1;
     "net.ipv4.tcp_slow_start_after_idle" = 0;
+    "net.ipv4.tcp_notsent_lowat" = 16384;
+    "kernel.numa_balancing" = 0;
   };
 }
