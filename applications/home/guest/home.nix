de872ce737e6509a5c001fd19fc273250f7f57ef
davi2012-dev <DaviMigue@proton.me>
2026-07-03 17:26:20 -0300

    Create home.nix

---
+++ applications/home/guest/home.nix
@@ -0,0 +1,40 @@
+{ pkgs, config, ... }:
+
+{
+  xdg.enable = true;
+
+  home.username = "guest";
+  home.homeDirectory = "/home/guest";
+
+  home.sessionVariables = {
+    XDG_SCREENSHOTS_DIR = "${config.home.homeDirectory}/Pictures";
+    XDG_PICTURES_DIR = "${config.home.homeDirectory}/Pictures";
+  };
+
+  #imports = [
+    ./librewolf.nix
+    ./mpv.nix
+    ./spotify.nix
+  ];
+
+  xsession.preferStatusNotifierItems = true;
+  xsession.numlock.enable = true;
+
+  services.blanket.enable = true;
+  services.amberol.enable = true;
+
+  home.packages = with pkgs; [
+    firefox
+    vlc
+    libreoffice
+    kate
+    ark
+    gwenview
+    wget
+    tree
+  ];
+
+  programs.home-manager.enable = true;
+
+  home.stateVersion = "25.11";
+}
