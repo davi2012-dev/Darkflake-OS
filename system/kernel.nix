63e15f6b37d2cc48ca10048fde4b268fbe465472
davi2012-dev <DaviMigue@proton.me>
2026-07-03 12:07:09 -0300

    Update kernel.nix

--- system/kernel.nix
+++ system/kernel.nix
@@ -95,8 +95,6 @@
     "vm.mmap_min_addr" = 65536;
     "net.core.rmem_max" = 16777216;
     "net.core.wmem_max" = 16777216;
-    "net.ipv4.tcp_rmem" = "4096 87380 16777216";
-    "net.ipv4.tcp_wmem" = "4096 65536 16777216";
     "net.ipv4.tcp_mem" = "786432 1048576 1572864";
     "net.ipv4.udp_mem" = "786432 1048576 1572864";
     "abi.vsyscall32" = 0;
