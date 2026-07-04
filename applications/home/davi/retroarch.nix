16340062ff77f5763498b878578def482854bb9c
davi2012-dev <DaviMigue@proton.me>
2026-07-04 10:56:52 -0300

    Update retroarch.nix

--- applications/home/davi/retroarch.nix
+++ applications/home/davi/retroarch.nix
@@ -1,3 +1,27 @@
-(  config, pkgs, ) 
+{ config, pkgs, ... }: {
+  programs.retroarch = {
+    enable = true;
+    
+    cores = {
+      # Game Boy Advance
+      mgba.enable = true;  
 
-(
+      # Super Nintendo (versão 2010)
+      snes9x = {
+        enable = true;
+        package = pkgs.libretro.snes9x2010;
+      };
+
+      genesis-plus-gx.enable = true;
+      gambatte.enable = true;
+      mupen64plus.enable = true;
+      np2kai.enable = true;
+      beetle-saturn.enable = true;
+    };
+
+    settings = {
+      video_driver = "vulkan";
+      video_fullscreen = "true";
+    };
+  };
+}
