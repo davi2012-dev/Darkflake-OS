df1ba73a0b1468004fa9b30964f6f36442bb1dcd
davi2012-dev <DaviMigue@proton.me>
2026-07-05 12:13:20 -0300

    Update xen.nix

--- virtualization/xen.nix
+++ virtualization/xen.nix
@@ -7,9 +7,9 @@
       memory = 4096;        
       maxVCPUs = 4;        
     };
-    boot.params = ''
-      dom0_mem=4096M dom0_max_vcpus=4
-      iommu=on intel_iommu=on
-    '';
+    boot.params = [
+      "dom0_mem=4096M dom0_max_vcpus=4"
+      "iommu=on intel_iommu=on"
+    ];
   };
 }
