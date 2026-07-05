3ecf16c686aac532f6495e6571592fc0b1a31be6
davi2012-dev <DaviMigue@proton.me>
2026-07-05 12:08:21 -0300

    Create xen.nix

---
+++ virtualization/xen.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+  virtualisation.xen = {
+    enable = true;
+    dom0Resources = {
+      memory = 4096;        
+      maxVCPUs = 4;        
+    };
+    boot.params = ''
+      dom0_mem=4096M dom0_max_vcpus=4
+      iommu=on intel_iommu=on
+    '';
+  };
+}
