78db8c0d35384bfc2916f6fd0eb9176a4f2a84ac
davi2012-dev <DaviMigue@proton.me>
2026-07-05 12:31:14 -0300

    Update bundle.nix

--- virtualization/bundle.nix
+++ virtualization/bundle.nix
@@ -1,14 +1,14 @@
 { ... }: {
   imports = [
     ./podman.nix
-    ./libvirtd.nix
     ./waydroid.nix
     ./lxc.nix
-    ./xen.nix
   ];
 
   virtualisation.appvm.enable = true;
   virtualisation.appvm.user = "davi";
   virtualisation.vswitch.enable = true;
   virtualisation.vswitch.resetOnStart = false;
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
 }
