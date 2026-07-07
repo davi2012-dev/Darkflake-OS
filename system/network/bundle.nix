67231d453131159f334dc69e45e54f644725e638
davi2012-dev <DaviMigue@proton.me>
2026-07-07 10:48:46 -0300

    Update bundle.nix

--- system/network/bundle.nix
+++ system/network/bundle.nix
@@ -1,62 +1,49 @@
 { config, pkgs, ... }: { 
 
-   imports = [ 
-     ./ssh.nix 
-     ./dns.nix 
-     ./samba.nix 
-     ./cockpit.nix 
-     ./firewall.nix 
-     ./analysis.nix  
-     ./Adguard.nix 
-     ./searxng.nix   
-     ./caddy.nix
-   ]; 
+  imports = [ 
+    ./ssh.nix 
+    ./dns.nix 
+    ./samba.nix 
+    ./cockpit.nix 
+    ./firewall.nix 
+    ./analysis.nix  
+    ./Adguard.nix 
+    ./searxng.nix   
+    ./caddy.nix
+  ]; 
 
-   # --- IDENTIFICAÇÃO DO SISTEMA ---
-   networking.hostName = "Darkflake"; 
+  networking.hostName = "Darkflake"; 
+  networking.stevenblack.enable = true;
+  networking.tempAddresses = "default";
+  networking.nameservers = [ 
+    "127.0.0.1" 
+    "::1" 
+  ]; 
+  networking.networkmanager.dns = "none"; 
+  networking.resolvconf.enable = false; 
+  services.resolved.enable = false; 
 
-   # --- BLOQUEIO NATIVO DE MALWARE/ADS (Steven Black) ---
-   networking.stevenblack.enable = true;
+  networking.networkmanager = { 
+    enable = true; 
+    wifi.backend = "iwd"; 
+    wifi.macAddress = "random";
+    ethernet.macAddress = "random";
+  }; 
 
-   # --- PRIVACIDADE E ROTEAMENTO ---
-   networking.tempAddresses = "default"; # Ativa IPs temporários/aleatórios no IPv6
+  services.timesyncd.enable = false;
 
-   # --- CONFIGURAÇÃO DE DNS ESTRITA ---
-   networking.nameservers = [ 
-     "127.0.0.1" 
-     "::1" 
-   ]; 
-   networking.networkmanager.dns = "none"; 
-   networking.resolvconf.enable = false; 
-   services.resolved.enable = false; 
-
-   # --- GERENCIAMENTO DE REDE (NetworkManager + IWD) ---
-   networking.networkmanager = { 
-     enable = true; 
-     wifi.backend = "iwd"; 
-     wifi.macAddress = "random";     # MAC Aleatório no Wi-Fi
-     ethernet.macAddress = "random"; # MAC Aleatório no Cabo
-   }; 
-
-   # --- CONFIGURAÇÃO ULTRA-SEGURA DE HORA (NTS) ---
-   
-   # 1. Desativar o cliente de hora padrão do systemd
-   services.timesyncd.enable = false; 
-
-   # 2. Chrony com NTS (Puxa a hora da internet com criptografia)
-   services.chrony = {
-     enable = true;
-     servers = [ 
-       "time.cloudflare.com iburst nts"
-       "nts.ekspresso.se iburst nts"
-       "a.st1.ntp.br iburst nts"
-       "b.st1.ntp.br iburst nts"
-       "nts.netnod.se iburst nts"
-       "ptbtime1.ptb.de iburst nts"
-     ];
-     extraConfig = ''
-       # Corrige a hora bruscamente se o erro for maior que 1s nos 3 primeiros boots
-       makestep 1.0 3
-     '';
-   };
+  services.chrony = {
+    enable = true;
+    servers = [
+      "time.cloudflare.com iburst nts"
+      "nts.netnod.se iburst nts"
+      "nts.ekspresso.se iburst nts"
+    ];
+    extraConfig = ''
+      server a.st1.ntp.br iburst
+      server b.st1.ntp.br iburst
+      server ptbtime1.ptb.de iburst
+      makestep 1.0 3
+    '';
+  };
 }
