d36ef83534cfc0d6e00819eb762214076dfc1a09
davi2012-dev <DaviMigue@proton.me>
2026-07-08 15:49:20 -0300

    Create dnscrypt-proxy.nix

---
+++ system/network/dnscrypt-proxy.nix
@@ -0,0 +1,62 @@
+{ config, pkgs, ... }:
+
+{
+  services.dnscrypt-proxy2 = {
+    enable = true;
+
+    settings = {
+      listen_addresses = [
+        "127.0.0.1:5353"
+        "[::1]:5353"
+      ];
+
+      server_names = [
+        "cloudflare"
+        "quad9-dnscrypt-main"
+        "scaleway-fr"
+      ];
+
+      require_nofilter = true;
+      require_nolog = true;
+      require_dnssec = true;
+
+      anonymized_dns = {
+        routes = [
+          { server_name = "cloudflare"; via = [ "anon-cs-de" ]; }
+          { server_name = "quad9-dnscrypt-main"; via = [ "anon-cs-nl" ]; }
+        ];
+      };
+
+      force_tcp = false;
+      ipv4_servers = true;
+      ipv6_servers = false;
+
+      cache = true;
+      cache_size = 4096;
+    };
+  };
+
+  systemd.services.dnscrypt-proxy.serviceConfig = {
+    ProtectSystem = "strict";
+    ProtectHome = true;
+    PrivateTmp = true;
+    ProtectControlGroups = true;
+    ProtectKernelModules = true;
+    ProtectKernelTunables = true;
+    NoNewPrivileges = true;
+    RestrictNamespaces = true;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+    MemoryDenyWriteExecute = true;
+
+    CapabilityBoundingSet = [ "" ];
+    AmbientCapabilities = [ "" ];
+
+    SystemCallArchitectures = "native";
+    SystemCallFilter = [
+      "@system-service"
+      "~@privileged"
+      "~@resources"
+    ];
+  };
+}
