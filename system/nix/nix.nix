5b58f3f1a93ba9e6241ab18f5d16b9b051a868f0
davi2012-dev <DaviMigue@proton.me>
2026-07-11 18:56:38 -0300

    Update nix.nix

--- system/nix/nix.nix
+++ system/nix/nix.nix
@@ -1,7 +1,12 @@
-{ config, pkgs, lib, inputs, ... }: {
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+{
 
-
-  # ---  NH: O Assistente Moderno ---
   programs.nh = {
     enable = true;
     clean.enable = true;
@@ -9,13 +14,19 @@
     flake = "/etc/nixos";
   };
 
-# ---  Nix Config & Performance ---
   nix.channel.enable = true;
   nix.settings = {
-    experimental-features = [ "nix-command" "flakes" "ca-derivations" "pipe-operators" "recursive-nix" "dynamic-derivations" "fetch-tree"  ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+      "ca-derivations"
+      "pipe-operators"
+      "recursive-nix"
+      "dynamic-derivations"
+      "fetch-tree"
+    ];
     auto-optimise-store = true;
-    
-    # Lista de substitutos limpa e otimizada
+
     substituters = [
       "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
@@ -23,10 +34,10 @@
       "https://cache.xinux.uz"
       "https://chaotic-nyx.cachix.org"
       "https://noctalia.cachix.org"
-      "https://guixpkgs.cachix.org" 
+      "https://guixpkgs.cachix.org"
+      "https://nix-gaming.cachix.org"
     ];
-    
-    # Chaves públicas corrigidas (incluindo a chave real do guixpkgs)
+
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
@@ -34,27 +45,45 @@
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "guixpkgs.cachix.org-1:rM4xwCs5NUy+FcCKkiWP/CmRaSVxxDPaKWZvM1bRopg="
       "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
-    
-    max-substitution-jobs = 20;
+
+    max-substitution-jobs = 30;
     builders-use-substitutes = true;
     sandbox = true;
-    cores = 0; 
+    cores = 0;
     max-jobs = "auto";
     http-connections = 50;
     connect-timeout = 5;
   };
 
-  # --- Nix-LD (Suporte a Binários Externos) ---
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
-    stdenv.cc.cc zlib fuse3 openssl icu nss curl expat glibc 
+    stdenv.cc.cc
+    zlib
+    fuse3
+    openssl
+    icu
+    nss
+    curl
+    expat
+    glibc
   ];
 
   environment.systemPackages = with pkgs; [
-    nvd nix-output-monitor
-    git gcc gnumake pkg-config
-    (python3.withPackages (ps: with ps; [ pip virtualenv ]))
+    nvd
+    nix-output-monitor
+    git
+    gcc
+    gnumake
+    pkg-config
+    (python3.withPackages (
+      ps: with ps; [
+        pip
+        virtualenv
+      ]
+    ))
     rustup
   ];
 
