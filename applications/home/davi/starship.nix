fc332a9439b80b6d40fc51f4a3ade9a0397009d1
davi2012-dev <DaviMigue@proton.me>
2026-07-11 10:36:04 -0300

    Update starship.nix

--- applications/home/davi/starship.nix
+++ applications/home/davi/starship.nix
@@ -7,19 +7,19 @@
 
     settings = {
       format = "$character$directory([ $git_branch$git_status ](bg:grey fg:black))$python$nix_shell$nodejs ❱ ";
-      add_newline = false;
+      add_newline = true;
       follow_symlinks = true;
       palette = "nordfox";
       palettes."nordfox" = {
-        black   = "#232831";  # bg0
-        white   = "#ABB1BB";  # fg2
-        grey    = "#7E8188";  # fg3
-        cyan    = "#88C0D0";
-        green   = "#A3BE8C";
-        purple  = "#B48EAD";
-        red     = "#BF616A";
-        blue    = "#81A1C1";
-        yellow  = "#ebcb8b";
+        black = "#232831"; # bg0
+        white = "#ABB1BB"; # fg2
+        grey = "#7E8188"; # fg3
+        cyan = "#88C0D0";
+        green = "#A3BE8C";
+        purple = "#B48EAD";
+        red = "#BF616A";
+        blue = "#81A1C1";
+        yellow = "#ebcb8b";
       };
 
       character = {
