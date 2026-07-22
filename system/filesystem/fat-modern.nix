4e12e37cc02c7ea08d6eb98b33a0993ff6747bc1
davi2012-dev <DaviMigue@proton.me>
2026-07-22 17:32:14 -0300

    Delete system/filesystem/fat-modern.nix

--- system/filesystem/fat-modern.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }: {
-  boot.supportedFilesystems = [ "vfat" "exfat" ];
-
-  environment.systemPackages = with pkgs; [
-    dosfstools      # mkfs.vfat, fsck.vfat
-    exfatprogs      # O novo padrão para exFAT (mais rápido que o antigo fuse-exfat)
-    mtools          # Manipula discos MS-DOS sem precisar montar (útil pra scripts)
-  ];
-}
