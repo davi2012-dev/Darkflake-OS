a0456b3d27b881e81bb2f41c47e9670afe0dc3f7
davi2012-dev <DaviMigue@proton.me>
2026-07-22 17:32:38 -0300

    Delete system/filesystem/network-fs.nix

--- system/filesystem/network-fs.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }: {
-  boot.supportedFilesystems = [ "cifs" "nfs" ];
-
-  environment.systemPackages = with pkgs; [
-    cifs-utils # Para montar compartilhamentos Windows/Samba
-    nfs-utils  # Para montar compartilhamentos Linux
-  ];
-}
