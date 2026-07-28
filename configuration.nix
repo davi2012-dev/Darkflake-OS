7ad7b3c992cad19c53de16eb043fc798a37fcbcf
davi2012-dev <DaviMigue@proton.me>
2026-07-28 12:16:35 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -25,6 +25,7 @@
     ./applications/fun.nix       
   ];
 
-  
+  system.nixos.codeName = "Lynx";
+  system.nixos.variantName = "Darkflake-OS Edition Desktop";
   system.stateVersion = "25.11"; 
 }
