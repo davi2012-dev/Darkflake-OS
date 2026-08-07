757ea28469ff1937952c2c7a41620e51b68be5d3
davi2012-dev <DaviMigue@proton.me>
2026-08-07 17:48:22 -0300

    Update ssh.nix

--- system/network/ssh.nix
+++ system/network/ssh.nix
@@ -1,48 +1,48 @@
-{ config, pkgs, ... }: { 
-  services.openssh = { 
-    enable = true; 
-    startWhenNeeded = true; 
-
-    settings = { 
-      AllowTcpForwarding = false; 
-      X11Forwarding = false; 
-      AllowAgentForwarding = false; 
-      PermitTunnel = false; 
-
-      PasswordAuthentication = false; 
-      KbdInteractiveAuthentication = false; 
-      PermitRootLogin = "no"; 
-
-      ClientAliveInterval = 300; 
-      ClientAliveCountMax = 2; 
-
-      LoginGraceTime = 20; 
-      MaxAuthTries = 3; 
-      MaxSessions = 5; 
-      MaxStartups = "10:30:60"; 
-
-      LogLevel = "VERBOSE"; 
-
-      KexAlgorithms = [ 
-        "mlkem768x25519-sha256" 
-        "curve25519-sha256" 
-      ]; 
-
-      Ciphers = [ 
-        "aes256-gcm@openssh.com" 
-        "chacha20-poly1305@openssh.com" 
-      ]; 
-
-      Macs = [ 
-        "hmac-sha2-512-etm@openssh.com" 
-        "umac-128-etm@openssh.com" 
-      ]; 
-
-      VersionAddendum = "none"; 
-      PermitUserEnvironment = false; 
-      Compression = false; 
-      TCPKeepAlive = false; 
-      PubkeyAuthentication = true; 
-    }; 
-  }; 
+{ config, pkgs, ... }:
+{
+  services.openssh = {
+    enable = true;
+    startWhenNeeded = true;
+    ports = [ 2222 ];
+    settings = {
+      AllowTcpForwarding = false;
+      X11Forwarding = false;
+      AllowAgentForwarding = false;
+      PermitTunnel = false;
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      PermitEmptyPasswords = false;
+      ClientAliveInterval = 300;
+      ClientAliveCountMax = 2;
+      LoginGraceTime = 20;
+      MaxAuthTries = 3;
+      MaxSessions = 5;
+      MaxStartups = "10:30:60";
+      LogLevel = "VERBOSE";
+      UseDns = false;
+      AllowUsers = [ "davi" ]; 
+      PerSourcePenalties = "crash:3600s authfail:3600s max:86400s";
+      KexAlgorithms = [
+        "mlkem768x25519-sha256"
+        "curve25519-sha256"
+      ];
+      Ciphers = [
+        "aes256-gcm@openssh.com"
+        "chacha20-poly1305@openssh.com"
+      ];
+      Macs = [
+        "hmac-sha2-512-etm@openssh.com"
+        "umac-128-etm@openssh.com"
+      ];
+      HostKeyAlgorithms = [
+        "ssh-ed25519"
+      ];
+      VersionAddendum = "none";
+      PermitUserEnvironment = false;
+      Compression = false;
+      TCPKeepAlive = false;
+      PubkeyAuthentication = true;
+    };
+  };
 }
