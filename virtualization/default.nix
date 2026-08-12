d05ebbe8f495844d37e5a3f66aba9c0755766107
davi2012-dev <260253037+davi2012-dev@users.noreply.github.com>
2026-08-12 18:33:37 -0300

    Update default.nix

--- virtualization/default.nix
+++ virtualization/default.nix
@@ -1,11 +1,12 @@
 { config, pkgs, ... }: {
   imports = [
     ./podman.nix
-    ./waydroid.nix
     ./lxc.nix
   ];
 
   virtualisation.libvirtd.enable = true;
+  virtualisation.waydroid.enable = true;
+  virtualisation.waydroid.package = pkgs.waydroid-nftables;
   virtualisation.spiceUSBRedirection.enable = true;
   programs.virt-manager.enable = true;
 }
