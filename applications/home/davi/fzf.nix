3c09b44a4dcf8d05f2f93beba20ef7603f3c14ec
davi2012-dev <260253037+davi2012-dev@users.noreply.github.com>
2026-08-12 18:19:00 -0300

    Update fzf.nix

--- applications/home/davi/fzf.nix
+++ applications/home/davi/fzf.nix
@@ -1,9 +1,12 @@
 { ... }:
+
 {
   programs.fzf = {
     enable = true;
     enableFishIntegration = true;
     defaultCommand = "fd --hidden --strip-cwd-prefix --exclude '.git'";
+    fileWidgetCommand = "fd --type f --hidden --strip-cwd-prefix --exclude '.git'";
+    changeDirWidgetCommand = "fd --type d --hidden --strip-cwd-prefix --exclude '.git'";
     defaultOptions = [
       "--layout=reverse"
       "--cycle"
