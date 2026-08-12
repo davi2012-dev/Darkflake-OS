f7d709e78c8371934bc02e3b6969caa6437bff8b
davi2012-dev <260253037+davi2012-dev@users.noreply.github.com>
2026-08-12 18:16:06 -0300

    Update zoxide configuration and fish integration
    Added options for zoxide configuration and updated fish function.

--- applications/home/davi/zoxide.nix
+++ applications/home/davi/zoxide.nix
@@ -1,18 +1,23 @@
 { ... }:
+
 {
   programs.zoxide = {
     enable = true;
-
-
     enableFishIntegration = true;
+    options = [ "--cmd cd" ];
+  };
 
-    options = [ "--cmd cd" ];  
+  programs.fish.functions.zi = {
+    description = "cd interativo via zoxide (fzf)";
+    body = ''
+      set -l dir (zoxide query -i -- $argv)
+      and cd "$dir"
+    '';
   };
 
   home.shellAliases = {
-    z = "cd";                  
-    zi = "zoxide query -i";    
-    zl = "zoxide query -l";   
-    zr = "zoxide remove -i";   
+    z = "cd";       
+    zl = "zoxide query -l"; 
+    zr = "zoxide remove -i"; 
   };
 }
