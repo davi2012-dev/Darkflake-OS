8b101466c6ad1db3e819b102311db9a79027714f
davi2012-dev <260253037+davi2012-dev@users.noreply.github.com>
2026-08-13 10:27:56 -0300

    Update starship.nix

--- applications/home/davi/starship.nix
+++ applications/home/davi/starship.nix
@@ -1,65 +1,79 @@
 { ... }:
+
 {
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
-
     settings = {
-      format = "$character$directory([ $git_branch$git_status ](bg:grey fg:black))$python$nix_shell$nodejs ❱ ";
       add_newline = true;
       follow_symlinks = true;
-      palette = "nordfox";
-      palettes."nordfox" = {
-        black = "#232831"; # bg0
-        white = "#ABB1BB"; # fg2
-        grey = "#7E8188"; # fg3
-        cyan = "#88C0D0";
-        green = "#A3BE8C";
-        purple = "#B48EAD";
-        red = "#BF616A";
-        blue = "#81A1C1";
-        yellow = "#ebcb8b";
+      format = "$directory$git_branch$git_status$python$nix_shell$nodejs$character";
+      palette = "catppuccin_mocha";
+      palettes.catppuccin_mocha = {
+        rosewater = "#f5e0dc";
+        flamingo = "#f2cdcd";
+        pink = "#f5c2e7";
+        mauve = "#cba6f7";
+        red = "#f38ba8";
+        maroon = "#eba0ac";
+        peach = "#fab387";
+        yellow = "#f9e2af";
+        green = "#a6e3a1";
+        teal = "#94e2d5";
+        sky = "#89dceb";
+        sapphire = "#74c7ec";
+        blue = "#89b4fa";
+        lavender = "#b4befe";
+        text = "#cdd6f4";
+        subtext1 = "#bac2de";
+        subtext0 = "#a6adc8";
+        overlay2 = "#9399b2";
+        overlay1 = "#7f849c";
+        overlay0 = "#6c7086";
+        surface2 = "#585b70";
+        surface1 = "#45475a";
+        surface0 = "#313244";
+        base = "#1e1e2e";
+        mantle = "#181825";
+        crust = "#11111b";
       };
-
       character = {
-        success_symbol = "[ I ](bg:blue fg:bold black)";
-        error_symbol = "[ I ](bg:red fg:bold black)";
-        vimcmd_symbol = "[ N ](bg:green fg:bold black)";
-        vimcmd_replace_symbol = "[ R ](bg:purple fg:bold black)";
-        vimcmd_replace_one_symbol = "[ R ](bg:purple fg:bold black)";
-        vimcmd_visual_symbol = "[ V ](bg:yellow fg:bold black)";
+        success_symbol = "[❯](bold green)";
+        error_symbol = "[❯](bold red)";
+        vimcmd_symbol = "[❮](bold teal)";
+        vimcmd_replace_symbol = "[❮](bold mauve)";
+        vimcmd_replace_one_symbol = "[❮](bold mauve)";
+        vimcmd_visual_symbol = "[❮](bold yellow)";
       };
       directory = {
-        format = "[ $path$read_only ]($style)";
-        style = "bg:black fg:bold white";
-        truncation_length = 5;
-        truncation_symbol = ".../";
+        format = "[$path]($style) ";
+        style = "bold lavender";
+        truncation_length = 3;
+        truncation_symbol = "…/";
       };
       git_branch = {
-        format = "$symbol$branch";
-        symbol = "";
+        format = "[ $symbol$branch]($style) ";
+        symbol = " ";
+        style = "mauve";
       };
       git_status = {
-        format = "$all_status";
+        format = "[($all_status$ahead_behind)]($style) ";
+        style = "peach";
       };
-
       python = {
-        # ($virtualenv) means conditional
-        format = " [ $symbol$version(:$virtualenv) ]($style)";
-        style = "bg:blue fg:black";
-        symbol = " ";
+        format = "[ $symbol($version )]($style)";
+        style = "yellow";
+        symbol = " ";
       };
-
       nix_shell = {
-        format = " [ $symbol$state ]($style)";
-        style = "bg:purple fg:black";
-        symbol = " ";
+        format = "[ $symbol$state ]($style)";
+        style = "sapphire";
+        symbol = " ";
       };
-
       nodejs = {
-        format = " [ $symbol$version ]($style)";
-        style = "bg:green fg:black";
-        symbol = " ";
+        format = "[ $symbol($version )]($style)";
+        style = "green";
+        symbol = " ";
       };
     };
   };
