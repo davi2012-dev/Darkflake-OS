0dbc5568388ced5285fbed13e150f21ce7e8b290
davi2012-dev <260253037+davi2012-dev@users.noreply.github.com>
2026-08-17 18:53:49 -0300

    Update ananicy-cpp.nix

--- system/optimization/ananicy-cpp.nix
+++ system/optimization/ananicy-cpp.nix
@@ -5,8 +5,6 @@
   services.ananicy = {
     enable = true;
     package = pkgs.ananicy-cpp;
-    
-    # 2. Aqui está o segredo: usar as regras do CachyOS para performance máxima
     rulesProvider = pkgs.ananicy-rules-cachyos;
   };
 
