4a68b6c1d6b655d10f107047a43b135e6780a069
davi2012-dev <260253037+davi2012-dev@users.noreply.github.com>
2026-08-23 12:39:06 -0300

    Update ssh.nix

--- system/network/ssh.nix
+++ system/network/ssh.nix
@@ -1,55 +1,80 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, ... }: {
+  services.fail2ban = {
+    enable = true;
+    maxretry = 5;
+    bantime = "1h";
+    bantime-increment = {
+      enable = true;
+      multipliers = "1 2 4 8 16 32 64 128 256";
+      maxtime = "168h";  # 1 semana
+      overalljails = true;
+    };
+  };
+
   services.openssh = {
     enable = true;
     startWhenNeeded = true;
     openFirewall = true;
+    
     hostKeys = [
       { path = "/etc/ssh/ssh_host_ed25519_key"; type = "ed25519"; }
     ];
+    
     settings = {
-      AllowTcpForwarding = false;
-      X11Forwarding = config.services.xserver.enable;
-      AllowAgentForwarding = false;
-      PermitTunnel = false;
+      # Usuários
+      AllowUsers = [ "davi" ];
+      
+      # Autenticação
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
-      PermitRootLogin = "no";
       PermitEmptyPasswords = false;
+      PubkeyAuthentication = true;
+      
+      # Permissões
+      PermitRootLogin = "no";
+      X11Forwarding = config.services.xserver.enable;  
+      AllowTcpForwarding = false;
+      AllowAgentForwarding = false;
+      PermitTunnel = false;
+      PermitUserEnvironment = false;
+      
+      # Conexão
       ClientAliveInterval = 300;
       ClientAliveCountMax = 2;
       LoginGraceTime = 20;
       MaxAuthTries = 3;
       MaxSessions = 5;
       MaxStartups = "10:30:60";
+      
+      # Log
       LogLevel = "VERBOSE";
       UseDns = false;
-      AllowUsers = [ "davi" ];
+      TCPKeepAlive = false;
+      Compression = false;
       PerSourcePenalties = "crash:3600s authfail:3600s max:86400s";
+      VersionAddendum = "none";
       KexAlgorithms = [
-        "mlkem768x25519-sha256"
-        "curve25519-sha256"
+        "mlkem768x25519-sha256"      # Pós-quântico
+        "curve25519-sha256"          # Moderno
       ];
+      
       Ciphers = [
-        "aes256-gcm@openssh.com"
-        "chacha20-poly1305@openssh.com"
+        "aes256-gcm@openssh.com"     # AES com GCM
+        "chacha20-poly1305@openssh.com" # Stream cipher
       ];
+      
       Macs = [
         "hmac-sha2-512-etm@openssh.com"
         "umac-128-etm@openssh.com"
       ];
+      
       HostKeyAlgorithms = [ "ssh-ed25519" ];
-      VersionAddendum = "none";
-      PermitUserEnvironment = false;
-      Compression = false;
-      TCPKeepAlive = false;
-      PubkeyAuthentication = true;
     };
   };
 
   programs.mosh = {
     enable = true;
-    openFirewall = true; 
-    withUtempter = true;   
+    openFirewall = true;
+    withUtempter = true;
   };
 }
