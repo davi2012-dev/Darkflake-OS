7e97bf81087dba61077f7a2768e0e21027d9ccdc
davi2012-dev <260253037+davi2012-dev@users.noreply.github.com>
2026-08-23 14:04:54 -0300

    Update librewolf.nix

--- applications/home/davi/librewolf.nix
+++ applications/home/davi/librewolf.nix
@@ -29,15 +29,38 @@ in
   programs.librewolf = {
     enable = true;
     languagePacks = [ "pt-BR" ];
+    
     policies = {
+      DontCheckDefaultBrowser = true;
+      DisablePocket = true;
+      DisableAppUpdate = true;
       DisableTelemetry = true;
       Certificates.ImportEnterpriseRoots = true;
     };
+    
     profiles.darkflake = {
       id = 0;
       isDefault = true;
       name = "Darkflake";
       settings = {
+        "security.enable_tls" = true;
+        "security.tls.version.min" = 2;
+        "security.tls.version.max" = 4;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "browser.sessionstore.privacy_level" = 0;
+        "browser.newtabpage.activity-stream.enabled" = false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.selfsupport.url" = "";
+        "extensions.abuseReport.enabled" = false;
+        "datareporting.policy.dataSubmissionEnabled" = false;
+        "app.shield.optoutstudies.enabled" = false;
+        "app.normandy.enabled" = false;
+        "extensions.pocket.enabled" = false;
+        "dom.disable_window_flip" = true;
+        "dom.disable_window_move_resize" = true;
+        "browser.formfill.enable" = false;
+        "plugin.disable_full_page_plugin_for_types" = "application/pdf,application/fdf,application/xfdf,application/lso,application/lss,application/iqy,application/rqy,application/lsl,application/xlk,application/xls,application/xlt,application/pot,application/pps,application/ppt,application/dos,application/dot,application/wks,application/bat,application/ps,application/eps,application/wch,application/wcm,application/wb1,application/wb3,application/rtf,application/doc,application/mdb,application/mde,application/wbk,application/ad,application/adp";
         "webgl.disabled" = true;
         "privacy.resistFingerprinting" = false;
         "privacy.trackingprotection.enabled" = true;
@@ -60,8 +83,6 @@ in
       "application/vnd.mozilla.xul+xml" "text/mml"
       "x-scheme-handler/http" "x-scheme-handler/https"
     ];
-    settings = {
-      StartupNotify = "true";
-    };
+    settings.StartupNotify = "true";
   };
 }
