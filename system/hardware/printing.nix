efcd42e880c1fd9251de9f4d2e1325dea9ce39df
davi2012-dev <260253037+davi2012-dev@users.noreply.github.com>
2026-09-04 17:30:38 -0300

    Update printing.nix

--- system/hardware/printing.nix
+++ system/hardware/printing.nix
@@ -1,17 +1,7 @@
 { config, pkgs, ... }: {
-
-  # Cria /var/spool/cups automaticamente
-  systemd.tmpfiles.rules = [
-    "d /var/spool/cups 1777 root root -"
-  ];
-
-  # Impressão
   services.printing = {
     enable = true;
-    drivers = with pkgs; [ 
-      gutenprint gutenprintBin hplip hplipWithPlugin
-      brlaser brgenml1lpr canon-cups-ufr2 epson-escpr splix foo2zjs
-    ];
+    drivers = [ ];
     browsing = false;
     defaultShared = false;
     webInterface = false;
@@ -19,69 +9,57 @@
     openFirewall = false;
   };
 
-  # Hardening do CUPS
-  systemd.services.cups = {
-    overrideStrategy = "asDropin";
-    serviceConfig = {
-      DynamicUser = true;
-      StateDirectory = "cups";
-      RuntimeDirectory = "cups";
-      CacheDirectory = "cups";
-      LogsDirectory = "cups";
-      ReadWritePaths = [ "/var/spool/cups" "/tmp" ];
+  services.ipp-usb.enable = true;
 
-      ProtectSystem = "strict";
-      ProtectHome = true;
-      PrivateTmp = true;
-      ProtectControlGroups = true;
-      ProtectKernelModules = true;
-      ProtectKernelTunables = true;
-      NoNewPrivileges = true;
-
-      CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" "CAP_DAC_OVERRIDE" ];
-      AmbientCapabilities = [ ];
-
-      RestrictNamespaces = "~user pid net uts cgroup ipc";
-      RestrictRealtime = true;
-      RestrictSUIDSGID = true;
-      MemoryDenyWriteExecute = true;
-      SystemCallArchitectures = "native";
-
-      DeviceAllow = [
-        "char-lp rw"
-        "char-usb/lp rw"
-        "/dev/bus/usb rw"
-      ];
-      DevicePolicy = "closed";
-
-      RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_UNIX" "AF_NETLINK" ];
-      PrivateNetwork = false;
-
-      SystemCallFilter = [
-        "~@raw-io" "~@clock" "~@reboot"
-        "~@swap" "~@mount" "~@module"
-      ];
-    };
+  hardware.printers = {
+    ensureDefaultPrinter = "MinhaImpressora";
+    ensurePrinters = [
+      {
+        name = "MinhaImpressora";
+        location = "Home";
+        deviceUri = "ipp://localhost/ipp/print";
+        model = "everywhere";
+      }
+    ];
   };
 
-  # Descoberta de impressoras na rede
-  services.avahi = {
-    enable = true;
-    domainName = "local";
-    allowInterfaces = [ "wlan0" ]; 
-    nssmdns4 = true;
-    nssmdns6 = true;
-    reflector = false;
-    publish = {
-      enable = true;
-      addresses = true;
-      workstation = true;
-    };
-  };
-
-  # Scanners
-  hardware.sane = {
-    enable = true;
-    extraBackends = [ pkgs.hplipWithPlugin ];
+  systemd.services.cups.serviceConfig = {
+    DynamicUser = true;
+    StateDirectory = "cups";
+    RuntimeDirectory = "cups";
+    CacheDirectory = "cups";
+    LogsDirectory = "cups";
+    ReadWritePaths = [ "/var/spool/cups" "/tmp" ];
+    ProtectSystem = "strict";
+    ProtectHome = true;
+    PrivateTmp = true;
+    ProtectControlGroups = true;
+    ProtectKernelModules = true;
+    ProtectKernelTunables = true;
+    NoNewPrivileges = true;
+    CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" ];
+    AmbientCapabilities = [ ];
+    RestrictNamespaces = "~user pid net uts cgroup ipc";
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+    MemoryDenyWriteExecute = true;
+    SystemCallArchitectures = "native";
+    DevicePolicy = "closed";
+    RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_UNIX" "AF_NETLINK" ];
+    SystemCallFilter = [ "~@raw-io" "~@clock" "~@reboot" "~@swap" "~@mount" "~@module" ];
   };
 }
+
+services.avahi = {
+  enable = true;
+  domainName = "local";
+  allowInterfaces = [ "wlan0" ];
+  nssmdns4 = true;
+  nssmdns6 = true;
+  reflector = false;
+  publish = {
+    enable = true;
+    addresses = true;
+    workstation = true;
+  };
+};
