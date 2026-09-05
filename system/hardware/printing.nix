8fe66c8aa00a127b75921d607fde6ca0341a8245
davi2012-dev <260253037+davi2012-dev@users.noreply.github.com>
2026-09-05 08:48:43 -0300

    Update printing.nix

--- system/hardware/printing.nix
+++ system/hardware/printing.nix
@@ -1,8 +1,13 @@
 { config, pkgs, ... }: {
+  systemd.tmpfiles.rules = [
+    "d /var/spool/cups 1777 root root -"
+  ];
+
   services.printing = {
     enable = true;
     drivers = [ ];
     browsing = false;
+    browsed.enable = false;
     defaultShared = false;
     webInterface = false;
     startWhenNeeded = true;
@@ -15,9 +20,15 @@
     ensureDefaultPrinter = "MinhaImpressora";
     ensurePrinters = [
       {
-        name = "MinhaImpressora";
-        location = "Home";
-        deviceUri = "ipp://localhost/ipp/print";
+        name = "MinhaImpressoraUSB";
+        location = "Home (USB)";
+        deviceUri = "ipp://127.0.0.1:60000/ipp/print";
+        model = "everywhere";
+      }
+      {
+        name = "MinhaImpressoraRede";
+        location = "Home (rede)";
+        deviceUri = "ipp://192.168.1.XX/ipp";
         model = "everywhere";
       }
     ];
@@ -48,18 +59,38 @@
     RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_UNIX" "AF_NETLINK" ];
     SystemCallFilter = [ "~@raw-io" "~@clock" "~@reboot" "~@swap" "~@mount" "~@module" ];
   };
-}
 
-services.avahi = {
-  enable = true;
-  domainName = "local";
-  allowInterfaces = [ "wlan0" ];
-  nssmdns4 = true;
-  nssmdns6 = true;
-  reflector = false;
-  publish = {
-    enable = true;
-    addresses = true;
-    workstation = true;
+  systemd.services.ipp-usb.serviceConfig = {
+    ProtectSystem = "strict";
+    ProtectHome = true;
+    PrivateTmp = true;
+    ProtectControlGroups = true;
+    ProtectKernelModules = true;
+    ProtectKernelTunables = true;
+    NoNewPrivileges = true;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+    MemoryDenyWriteExecute = true;
+    SystemCallArchitectures = "native";
+    RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_UNIX" ];
   };
-};
+
+  services.avahi = {
+    enable = true;
+    domainName = "local";
+    allowInterfaces = [ "wlan0" ];
+    nssmdns4 = true;
+    nssmdns6 = true;
+    reflector = false;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+  };
+
+  hardware.sane = {
+    enable = true;
+    extraBackends = [ pkgs.hplipWithPlugin ];
+  };
+}
