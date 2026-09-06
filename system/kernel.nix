8c5f9c6288a1701a2921ab0873263f0adac46a01
davi2012-dev <260253037+davi2012-dev@users.noreply.github.com>
2026-09-06 12:17:56 -0300

    Remove SGX and Brillo hardware settings

--- system/kernel.nix
+++ system/kernel.nix
@@ -210,8 +210,6 @@
   hardware.cpu.x86.msr.settings.allow-writes = "off";
   hardware.cpu.x86.msr.mode = "0640";
   environment.memoryAllocator.provider = "mimalloc";
-  hardware.cpu.intel.sgx.enableDcapCompat = true;
-  hardware.brillo.enable = true;
   hardware.amdgpu.zluda.enable = true;
   hardware.amdgpu.opencl.enable = true;
   hardware.intel-gpu-tools.enable = true;
